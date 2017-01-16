688d8ed3b88f76d5dc77016bd9c0a7fab76b5279
Chris Wynn <chrisw@MacBook-Pro-2.local>
2017-01-16 12:22:34 -0700

    Added Groups and Dashboard placeholders
    Added Groups and Dashboard placeholders.

--- dmm-crm-plugin.php
+++ dmm-crm-plugin.php
@@ -4,7 +4,7 @@
  * Plugin URI: https://github.com/ChasmSolutions/DMM-CRM-Plugin
  * Description: DMM CRM is a contact relationship management system for disciple making movements. 
  * Version: 0.0.1
- * Author: Chasm Solutions
+ * Author: Chasm.Solutions & Kingdom.Training
  * Author URI: https://github.com/ChasmSolutions
  *
  * @package   DmmCrm
@@ -58,7 +58,28 @@ $dmmcrm_post_type_contacts = new Dmmcrm_Contacts_Post_Type;
  */
 $dmmcrm_post_type_locations = new Dmmcrm_Locations_Post_Type;
 
+/**
+ * Groups Post Type
+ *
+ * This defines the Groups custom post type. A majority of the discovery groups
+ * and simple church app data will be stored under this custom post type.
+ * Taxonomy and heavy use of meta are used as well to construct the different 
+ * data functionalities that this plugin provides.
+ *
+ * @since 0.0.1
+ */
+$dmmcrm_post_type_groups = new Dmmcrm_Groups_Post_Type;
 
+/**
+ * Dashboard Widgets
+ *
+ * This defines the default dashboard widgets.
+ * 
+ * TODO: Convert to Class & Convert placeholder content.
+ *
+ * @since 0.0.1
+ */
+require_once( DMMCRM_PLUGIN_DIR . "includes/dmmcrm-dashboard.php" );
 
 
 
