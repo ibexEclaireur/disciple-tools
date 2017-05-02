0b525b4714e7e5b350653815726a3cda1f2c972b
Chris Wynn <chris@chasm.solutions>
2017-05-02 10:47:03 -0600

    Added a class check for analytics
    Added a class check for the analytics bundle in the disciple-tools.php
    file. It just checks if the GA plugin is enabled and disables our
    bundle so there is no crash conflict. We should build an alert that
    requests that they disable the GA plugin for our plugin to work
    properly.

---
+++ includes/activity/activity-metabox.php
@@ -0,0 +1,5 @@
+<?php
+/**
+ * Creates the metaboxes and lists for contacts and groups activity.
+ */
+
--- includes/admin/multi-role/inc/functions-options.php
+++ includes/admin/multi-role/inc/functions-options.php
@@ -10,16 +10,6 @@
  * @license    http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
  */
 
-/**
- * Conditional check to see if the role manager is enabled.
- *
- * @since  1.0.0
- * @access public
- * @return bool
- */
-function dt_multi_role_role_manager_enabled() {
-	return apply_filters( 'dt_multi_role_role_manager_enabled', dt_multi_role_get_setting( 'role_manager' ) );
-}
 
 /**
  * Conditional check to see if denied capabilities should overrule granted capabilities when
@@ -30,7 +20,7 @@ function dt_multi_role_role_manager_enabled() {
  * @return bool
  */
 function dt_multi_role_explicitly_deny_caps() {
-	return apply_filters( 'dt_multi_role_explicitly_deny_caps', dt_multi_role_get_setting( 'explicit_denied_caps' ) );
+	return apply_filters( 'dt_multi_role_explicitly_deny_caps', false );
 }
 
 /**
@@ -44,80 +34,4 @@ function dt_multi_role_multiple_user_roles_enabled() {
 	return apply_filters( 'dt_multi_role_multiple_roles_enabled', true );
 }
 
-/**
- * Conditional check to see if content permissions are enabled.
- *
- * @since  1.0.0
- * @access public
- * @return bool
- */
-function dt_multi_role_content_permissions_enabled() {
-	return apply_filters( 'dt_multi_role_content_permissions_enabled', dt_multi_role_get_setting( 'content_permissions' ) );
-}
 
-/**
- * Conditional check to see if login widget is enabled.
- *
- * @since  1.0.0
- * @access public
- * @return bool
- */
-function dt_multi_role_login_widget_enabled() {
-	return apply_filters( 'dt_multi_role_login_widget_enabled', dt_multi_role_get_setting( 'login_form_widget' ) );
-}
-
-/**
- * Conditional check to see if users widget is enabled.
- *
- * @since  1.0.0
- * @access public
- * @return bool
- */
-function dt_multi_role_users_widget_enabled() {
-	return apply_filters( 'dt_multi_role_users_widget_enabled', dt_multi_role_get_setting( 'users_widget' ) );
-}
-
-/**
- * Gets a setting from from the plugin settings in the database.
- *
- * @since  0.2.0
- * @access public
- * @return mixed
- */
-function dt_multi_role_get_setting( $option = '' ) {
-
-	$defaults = dt_multi_role_get_default_settings();
-
-	$settings = wp_parse_args( get_option( 'dt_multi_role_settings', $defaults ), $defaults );
-
-	return isset( $settings[ $option ] ) ? $settings[ $option ] : false;
-}
-
-/**
- * Returns an array of the default plugin settings.
- *
- * @since  0.2.0
- * @access public
- * @return array
- */
-function dt_multi_role_get_default_settings() {
-
-	return array(
-
-		// @since 0.1.0
-		'role_manager'        => 1,
-		'content_permissions' => 1,
-		'private_blog'        => 0,
-
-		// @since 0.2.0
-		'private_feed'              => 0,
-		'login_form_widget'         => 0,
-		'users_widget'              => 0,
-		'content_permissions_error' => esc_html__( 'Sorry, but you do not have permission to view this content.', 'members' ),
-		'private_feed_error'        => esc_html__( 'You must be logged into the site to view this content.',      'members' ),
-
-		// @since 1.0.0
-		'explicit_denied_caps' => true,
-		'multi_roles'          => true,
-	);
-}
