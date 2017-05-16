58601b4b3f38f141f3bcb8bee746ac35f05adea8
corsac <corsac@hushmail.com>
2017-05-16 13:13:07 +0100

    interface for generating API keys

--- includes/admin/class-api-keys.php
+++ includes/admin/class-api-keys.php
@@ -1,61 +1,149 @@
 <?php
-if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+} // Exit if accessed directly
 
 /**
  * Class Disciple_Tools_Api_Keys
  * Generate api keys for DT. The api key can be used by external sites or
  * applications where there is no authenticated user.
  */
-class Disciple_Tools_Api_Keys
-{
-  /**
-   * @var object instance. The class instance
-   * @access private
-   * @since 0.1
-   */
-  private static $_instance = null;
+class Disciple_Tools_Api_Keys {
+	/**
+	 * @var object instance. The class instance
+	 * @access private
+	 * @since 0.1
+	 */
+	private static $_instance = null;
 
-  /**
+	/**
 	 * Main Disciple_Tools_Api_Keys Instance
 	 * Ensures only one instance of Disciple_Tools_Api_Keys is loaded or can be loaded.
 	 * @since 0.1
 	 * @static
 	 * @return Disciple_Tools_Api_Keys instance
 	 */
-  public static function instance () {
-    if ( is_null( self::$_instance ) )
-      self::$_instance = new self();
-    return self::$_instance;
-  } // End instance()
+	public static function instance() {
+		if ( is_null( self::$_instance ) ) {
+			self::$_instance = new self();
+		}
 
-  /**
+		return self::$_instance;
+	} // End instance()
+
+	/**
 	 * Constructor function.
 	 * @access  public
 	 * @since   0.1
 	 */
-  public function __construct (){
-    add_action('admin_menu', array($this, 'add_api_keys_menu'));
-  }
+	public function __construct() {
+		add_action( 'admin_menu', array( $this, 'add_api_keys_menu' ) );
+	}
 
 	/**
 	 * Create the menu item in the Tools section
-   * @access public
-   * @since 0.1
+	 * @access public
+	 * @since 0.1
 	 */
-  public function add_api_keys_menu(){
-    add_submenu_page('tools.php', __('API Keys (DT)', 'disciple_tools'),
-      __('API Keys (DT)', 'disciple_tools'), 'manage_options', 'api-keys', array($this, 'api_keys_page'));
-  }
+	public function add_api_keys_menu() {
+		add_submenu_page( 'tools.php', __( 'API Keys (DT)', 'disciple_tools' ),
+			__( 'API Keys (DT)', 'disciple_tools' ), 'manage_options', 'api-keys', array( $this, 'api_keys_page' ) );
+	}
 
-  /**
-   * The API keys page html
-   * @access public
-   * @since 0.1
-   */
-  public function api_keys_page(){
-    ?>
-    <h1>API Keys</h1>
-    <?php
-  }
+	/**
+	 * Display an admin notice on the page
+	 *
+	 * @param $notice , the message to display
+	 * @param $type , the type of message to display
+	 *
+	 * @access private
+	 * @since 0.1
+	 */
+	private function admin_notice( $notice, $type ) {
+		echo '<div class="notice notice-' . $type . ' is-dismissible"><p>';
+		echo $notice;
+		echo '</p></div>';
+	}
+
+	/**
+	 * See if a value is in an array of values
+	 *
+	 * @param $needle , the value to find
+	 * @param $haystack , the array to look in
+	 * @param bool $strict
+	 *
+	 * @access private
+	 * @since 0.1
+	 *
+	 * @return bool
+	 */
+	private function in_array_r( $needle, $haystack, $strict = false ) {
+		foreach ( $haystack as $item ) {
+			if ( ( $strict ? $item === $needle : $item == $needle ) || ( is_array( $item ) && $this->in_array_r( $needle, $item, $strict ) ) ) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	/**
+	 * The API keys page html
+	 * @access public
+	 * @since 0.1
+	 */
+	public function api_keys_page() {
+		$keys = get_option( "dt_api_keys", array() );
+		if ( isset( $_POST["application"] ) ) {
+			$token = bin2hex( random_bytes( 32 ) );
+			if ( ! $this->in_array_r( $_POST["application"], $keys ) ) {
+				$keys[] = array( "client_id" => $_POST["application"], "client_token" => $token );
+				update_option( "dt_api_keys", $keys );
+			} else {
+				$this->admin_notice( "Application already exists", "error" );
+			}
+		}
+		?>
+      <div class="wrap">
+        <form action="" method="post">
+          <h1>Generate Token</h1>
+          <table class="">
+            <tr>
+              <th>
+                <label for="application">Application Name</label>
+              </th>
+              <td>
+                <input type="text" id="application" name="application">
+              </td>
+              <td>
+                <button type="submit">Generate Token</button>
+              </td>
+            </tr>
+          </table>
+        </form>
+        <h1>API Keys</h1>
+        <table>
+          <thead>
+          <tr>
+            <th>Client ID</th>
+            <th>Client Token</th>
+          </tr>
+          </thead>
+			<?php foreach ( $keys as $key ): ?>
+              <tbody>
+              <tr>
+                <td>
+					<?php echo $key["client_id"] ?>
+                </td>
+                <td>
+					<?php echo $key["client_token"] ?>
+                </td>
+              </tr>
+              </tbody>
+			<?php endforeach; ?>
+        </table>
+      </div>
+		<?php
+	}
 
 }
