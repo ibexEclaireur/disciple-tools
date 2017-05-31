c8496f05515a3db4c1c992fd83f5510712f1ff26
Chris Wynn <chris@chasm.solutions>
2017-05-31 13:16:13 -0600

    Moved functions into theme support folder

--- disciple-tools.php
+++ disciple-tools.php
@@ -350,12 +350,21 @@ class Disciple_Tools {
         require_once ('includes/factories/class-counter-factory.php');
         $this->counter = Disciple_Tools_Counter_Factory::instance();
 
-
         /**
          * Load Functions
          */
         require_once ('includes/functions/disable-xml-rpc-pingback.php');
 
+        /**
+         * Theme Support functions
+         */
+        require_once('includes/theme_support/user-functions-for-themes.php');
+        require_once('includes/theme_support/group-functions-for-themes.php');
+        require_once('includes/theme_support/contact-functions-for-themes.php');
+        require_once('includes/theme_support/location-functions-for-themes.php');
+        require_once('includes/theme_support/chart-functions-for-themes.php');
+
+        // Language
 		add_action( 'init', array( $this, 'load_plugin_textdomain' ) );
 
     } // End __construct()
--- includes/theme_support/sample-data.php
+++
@@ -1,5 +0,0 @@
-<?php
-/**
- * TODO: This file contains sample data used to accelerate development and should be removed for production
- */
-
