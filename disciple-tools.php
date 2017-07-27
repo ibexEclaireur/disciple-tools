a2cfe1e6d8173beaef29b25db4c09642c704d6ef
Burning Sun <burntsun@users.noreply.github.com>
2017-07-27 17:17:48 +0100

    Show admin warning on old PHP versions

--- disciple-tools.php
+++ disciple-tools.php
@@ -20,6 +20,28 @@
 // If this file is called directly, abort.
 if ( ! defined( 'ABSPATH' ) ) { exit; } // Exit if accessed directly
 
+function admin_notice_required_php_version_dt() {
+    ?>
+    <div class="notice notice-error">
+        <p><?php _e( "The Disciple Tools plug-in requires PHP 7.0 or greater before it will have any effect. Please upgrade your PHP version or uninstall this plugin." ); ?></p>
+    </div>
+    <?php
+}
+
+if (version_compare( phpversion(), '7.0', '<' )) {
+
+    /* We only support PHP >= 7.0, however, we want to support allowing users
+     * to install this plugin even on old versions of PHP, without showing a
+     * horrible message, but instead a friendly notice.
+     *
+     * For this to work, this file must be compatible with old PHP versions.
+     * Feel free to use PHP 7 features in other files, but not in this one.
+     */
+
+    add_action( 'admin_notices', 'admin_notice_required_php_version_dt' );
+    error_log( 'Disciple Tools plugin requires PHP version 7.0 or greater, please upgrade PHP or uninstall this plugin' );
+    return;
+}
 
 /**
  * Activation Hook
