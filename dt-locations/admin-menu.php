dc0297195a38bd4c80bb93ca5b854a49a98f8491
Chris Wynn <chris@chasm.solutions>
2017-09-04 11:25:56 -0600

    Setting up USA query.

--- dt-locations/admin-menu.php
+++ dt-locations/admin-menu.php
@@ -113,14 +113,17 @@ class Disciple_Tools_Location_Tools_Menu {
                 $html .= '<div class="wrap"><div id="poststuff"><div id="post-body" class="metabox-holder columns-2">';
                 $html .= '<div id="post-body-content">';
     
+                /* BOX */
                 $html .= '<table class="widefat striped"><thead><th>Title</th></thead><tbody><tr><td>';
+                
                 /*first column*/
                 $html .= '</td></tr><tr><td>';
                 /*second column*/
                 $html .= '</td></tr></tbody></table>';
                 
-                $html .= '<br>';
+                $html .= '<br>'; // separate boxes
     
+                /* BOX */
                 $html .= '<table class="widefat striped"><thead><th>Title</th></thead><tbody><tr><td>';
                 /*first column*/
                 $html .= '</td></tr><tr><td>';
@@ -129,6 +132,7 @@ class Disciple_Tools_Location_Tools_Menu {
                 
                 $html .= '</div><!-- end post-body-content --><div id="postbox-container-1" class="postbox-container">';
     
+                /* BOX */
                 $html .= '<table class="widefat striped"><thead><th>Title</th></thead><tbody><tr><td>';
                 /*first column*/
                 $html .= '</td></tr><tr><td>';
@@ -142,24 +146,28 @@ class Disciple_Tools_Location_Tools_Menu {
             case "usa":
                 $html .= '<div class="wrap"><div id="poststuff"><div id="post-body" class="metabox-holder columns-2">';
                 $html .= '<div id="post-body-content">';
-    
-                $html .= '<table class="widefat striped"><thead><th>Title</th></thead><tbody><tr><td>';
-                /*first column*/
-                $html .= '</td></tr><tr><td>';
-                /*second column*/
+                
+                /* BOX */
+                $html .= '<table class="widefat striped"><thead><th>Your US County</th></thead><tbody><tr><td>';
+                
+                $object = new Disciple_Tools_Locations_Tab_USA();
+                $county_id = ''; // post info
+                $html .= $object->process_install_us_county( $county_id );
+                $html .= $object->install_us_county();
+                
                 $html .= '</td></tr></tbody></table>';
     
                 $html .= '<br>';
     
-                $html .= '<table class="widefat striped"><thead><th>Title</th></thead><tbody><tr><td>';
-                /*first column*/
-                $html .= '</td></tr><tr><td>';
-                /*second column*/
+                /* BOX */
+                $html .= '<table class="widefat striped"><thead><th>Your State</th></thead><tbody><tr><td>';
+                
                 $html .= '</td></tr></tbody></table>';
     
                 $html .= '</div><!-- end post-body-content --><div id="postbox-container-1" class="postbox-container">';
     
-                $html .= '<table class="widefat striped"><thead><th>Title</th></thead><tbody><tr><td>';
+                /* BOX */
+                $html .= '<table class="widefat striped"><thead><th>Instructions</th></thead><tbody><tr><td>';
                 /*first column*/
                 $html .= '</td></tr><tr><td>';
                 /*second column*/
@@ -176,4 +184,223 @@ class Disciple_Tools_Location_Tools_Menu {
 
         echo $html; // Echo contents
     }
+}
+
+
+
+/**
+ * Creates a dropdown of the states with the state key as the value.
+ * @usage USA locations
+ *
+ * @return string
+ */
+function dt_get_states_key_dropdown_not_installed () {
+    
+    $dir_contents = dt_get_usa_meta();
+    
+    $dropdown = '<select name="states-dropdown">';
+    
+    foreach ($dir_contents->USA_states as $value) {
+        $disabled = '';
+        
+        $dropdown .= '<option value="' . $value->key . '" ';
+        if (get_option( '_installed_us_county_'.$value->key )) {$dropdown .= ' disabled';
+            $disabled = ' (Installed)';}
+        elseif (isset( $_POST['states-dropdown'] ) && $_POST['states-dropdown'] == $value->key) {$dropdown .= ' selected';}
+        $dropdown .= '>' . $value->name . $disabled;
+        $dropdown .= '</option>';
+    }
+    $dropdown .= '</select>';
+    
+    return $dropdown;
+}
+
+/**
+ * Creates a dropdown of the states with the state key as the value.
+ * @usage USA locations
+ *
+ * @return string
+ */
+function dt_get_states_key_dropdown_installed () {
+    
+    $dir_contents = dt_get_usa_meta(); // get directory & build dropdown
+    
+    $dropdown = '<select name="states-dropdown">';
+    
+    foreach ($dir_contents->USA_states as $value) {
+        $disabled = '';
+        
+        $dropdown .= '<option value="' . $value->key . '" ';
+        if (!get_option( '_installed_us_county_'.$value->key )) {$dropdown .= ' disabled';
+            $disabled = ' (Not Installed)';}
+        elseif (isset( $_POST['states-dropdown'] ) && $_POST['states-dropdown'] == $value->key) {$dropdown .= ' selected';}
+        $dropdown .= '>' . $value->name . $disabled;
+        $dropdown .= '</option>';
+    }
+    $dropdown .= '</select>';
+    
+    return $dropdown;
+}
+
+
+/**
+ * Get the master json file with USA states and counties names, ids, and file locations.
+ * @usage USA locations
+ *
+ * @return array|mixed|object
+ */
+function dt_get_usa_meta() {
+    return json_decode( file_get_contents( plugin_dir_path( __FILE__ ) . 'json/usa-meta.json' ) );
+}
+
+/**
+ * Get the master list of countries for omega zones including country abbreviation, country name, and zone.
+ * @return array|mixed|object
+ */
+function dt_get_oz_country_list( $admin = 'cnty' ) {
+    
+    switch ( $admin ) {
+        case 'cnty':
+            $result =  json_decode( file_get_contents( plugin_dir_path( __FILE__ ) . 'json/oz/oz_cnty.json' ) );
+            return $result->RECORDS;
+            break;
+        case 'admin1':
+            $result =  json_decode( file_get_contents( plugin_dir_path( __FILE__ ) . 'json/oz/oz_admin1.json' ) );
+            return $result->RECORDS;
+            break;
+        case 'admin2':
+            $result =  json_decode( file_get_contents( plugin_dir_path( __FILE__ ) . 'json/oz/oz_admin2.json' ) );
+            return $result->RECORDS;
+            break;
+        case 'admin3':
+            $result =  json_decode( file_get_contents( plugin_dir_path( __FILE__ ) . 'json/oz/oz_admin3.json' ) );
+            return $result->RECORDS;
+            break;
+        case 'admin4':
+            $result =  json_decode( file_get_contents( plugin_dir_path( __FILE__ ) . 'json/oz/oz_admin4.json' ) );
+            return $result->RECORDS;
+            break;
+        default:
+            break;
+    }
+    
+    return false;
+}
+
+
+/**
+ * Gets the meta information for a polygon or array of polygons
+ * @usage USA locations
+ *
+ * @param  $geoid        (int) Can be full 9 digit geoid or 5 digit state/county code
+ * @return array
+ */
+function dt_get_coordinates_meta ( $geoid ) {
+    global $wpdb;
+    
+    //* query */
+    $county_coords = $wpdb->get_results( "SELECT meta_value FROM $wpdb->postmeta WHERE meta_key LIKE 'polygon_$geoid%'", ARRAY_A );
+    
+    /* build full json of coodinates*/
+    $rows = count( $county_coords );
+    $string = '[';
+    $i = 0;
+    foreach($county_coords as $value) {
+        $string .= $value['meta_value'];
+        if($rows > $i + 1 ) {$string .= ','; }
+        $i++;
+    }
+    $string .= ']';
+    
+    $coords_objects = json_decode( $string );
+    
+    /* set values */
+    $high_lng_e = -9999999; //will hold max val
+    $high_lat_n = -9999999; //will hold max val
+    $low_lng_w = 9999999; //will hold max val
+    $low_lat_s = 9999999; //will hold max val
+    
+    /* filter for high and lows*/
+    foreach ($coords_objects as $coords) {
+        foreach($coords as $k=>$v)
+        {
+            if($v->lng > $high_lng_e)
+            {
+                $high_lng_e = $v->lng;
+            }
+            if($v->lng < $low_lng_w)
+            {
+                $low_lng_w = $v->lng;
+            }
+            if($v->lat > $high_lat_n)
+            {
+                $high_lat_n = $v->lat;
+            }
+            if($v->lat < $low_lat_s)
+            {
+                $low_lat_s = $v->lat;
+            }
+        }
+    }
+    //    print ' | n : '. $high_lat_n;
+    //    print ' | s : '. $low_lat_s;
+    //    print ' | e : '. $high_lng_e;
+    //    print ' | w : '. $low_lng_w;
+    
+    
+    // calculate centers
+    $lng_size = $high_lng_e - $low_lng_w;
+    $half_lng_difference = $lng_size / 2;
+    $center_lng = $high_lng_e - $half_lng_difference;
+    //    print ' | lng size: '.$lng_size ;
+    
+    $lat_size = $high_lat_n - $low_lat_s;
+    $half_lat_difference = $lat_size / 2;
+    $center_lat = $high_lat_n - $half_lat_difference;
+    //    print ' | lat size: '.$lat_size ;
+    
+    // get zoom level
+    if($lat_size > 3 || $lng_size > 3) {
+        $zoom = 6;
+    } elseif ($lat_size > 2 || $lng_size > 2) {
+        $zoom = 7;
+    } elseif ($lat_size > 1 || $lng_size > 1) {
+        $zoom = 8;
+    } elseif ($lat_size > .4 || $lng_size > .4) {
+        $zoom = 9;
+    } elseif ($lat_size > .2 || $lng_size > .2) {
+        $zoom = 10;
+    } elseif ($lat_size > .1 || $lng_size > .1) {
+        $zoom = 11;
+    } elseif ($lat_size > .07 || $lng_size > .07) {
+        $zoom = 12;
+    } elseif ($lat_size > .01 || $lng_size > .01) {
+        $zoom = 13;
+    } else {
+        $zoom = 14;
+    }
+    
+    //    print ' | zoom: '.$zoom ;
+    
+    $meta = ["center_lng" => (float) $center_lng,"center_lat" => (float) $center_lat,"ne" => $high_lat_n.','.$high_lng_e,"sw" => $low_lat_s.','.$low_lng_w ,"zoom" => (float) $zoom];
+    
+    return $meta;
+}
+
+/**
+ * Get the full country name from key
+ * @param $key
+ * @return mixed
+ */
+function dt_locations_match_country_to_key( $key ) {
+    
+    $countries = dt_get_oz_country_list();
+    
+    foreach($countries as $country) {
+        if($country->CntyID == $key) {
+            return $country->Cnty_Name;
+        }
+    }
+    
+    return false;
 }
--- dt-locations/admin-tab-usa.php
+++ dt-locations/admin-tab-usa.php
@@ -14,26 +14,11 @@ if ( ! defined( 'ABSPATH' ) ) { exit; } // Exit if accessed directly
 
 class Disciple_Tools_Locations_Tab_USA {
     
-    /**
-     * Page content for the tab
-     */
-    public function page_contents() {
-        
-        $html = '';
-        
-        $html .= '<div class="wrap"><h2>Import</h2>'; // Block title
-        
-        $html .= '<div class="wrap"><div id="poststuff"><div id="post-body" class="metabox-holder columns-2">';
-        $html .= '<div id="post-body-content">';
-        
-        $html .= '</div><!-- end post-body-content --><div id="postbox-container-1" class="postbox-container">';
-        
-        $html .= '</div><!-- postbox-container 1 --><div id="postbox-container-2" class="postbox-container">';
-        $html .= '';/* Add content to column */
-        
-        $html .= '</div><!-- postbox-container 2 --></div><!-- post-body meta box container --></div><!--poststuff end --></div><!-- wrap end -->';
-        
-        return $html;
-        
+    public function install_us_county() {
+        return '<form method="post"> <input type="text" name="county" class="text-small" /> <button type="submit" class="button">Install US County</button></form>'; // auto search field
+    }
+    
+    public function process_install_us_county( $county_id ) {
+        return $county_id;
     }
 }
