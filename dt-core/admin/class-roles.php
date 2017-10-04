d5fc16352bd9785e7518f0ebee247ef6a844bdac
Burning Sun <burntsun@users.noreply.github.com>
2017-10-04 09:54:10 +0100

    Don't use wpcom_vip_ functions
    They don't seem to be available in a normal installation.

--- dt-core/admin/class-roles.php
+++ dt-core/admin/class-roles.php
@@ -167,7 +167,7 @@ class Disciple_Tools_Roles
         if( get_role( 'strategist' ) ) {
             remove_role( 'strategist' );
         }
-        wpcom_vip_add_role(
+        add_role(
             'strategist', 'Strategist',
             [
                 /* Standard Capabilities */
@@ -289,7 +289,7 @@ class Disciple_Tools_Roles
         if( get_role( 'dispatcher' ) ) {
             remove_role( 'dispatcher' );
         }
-        wpcom_vip_add_role(
+        add_role(
             'dispatcher', 'Dispatcher',
             [
                 /* Standard Capabilities */
@@ -366,7 +366,7 @@ class Disciple_Tools_Roles
         if( get_role( 'marketer' ) ) {
             remove_role( 'marketer' );
         }
-        wpcom_vip_add_role(
+        add_role(
             'marketer', 'Marketer',
             [
                 /* Standard Capabilities */
@@ -486,7 +486,7 @@ class Disciple_Tools_Roles
         if( get_role( 'marketer_leader' ) ) {
             remove_role( 'marketer_leader' );
         }
-        wpcom_vip_add_role(
+        add_role(
             'marketer_leader', 'Marketer Leader',
             [
                 /* Standard Capabilities */
@@ -605,7 +605,7 @@ class Disciple_Tools_Roles
         if( get_role( 'multiplier' ) ) {
             remove_role( 'multiplier' );
         }
-        wpcom_vip_add_role(
+        add_role(
             'multiplier', 'Multiplier',
             [
                 'access_contacts'        => true,
@@ -618,7 +618,7 @@ class Disciple_Tools_Roles
         if( get_role( 'project_supporter' ) ) {
             remove_role( 'project_supporter' );
         }
-        wpcom_vip_add_role(
+        add_role(
             'project_supporter', 'Project Supporter',
             [
                 'project_supporter' => true,
@@ -630,7 +630,7 @@ class Disciple_Tools_Roles
         if( get_role( 'prayer_supporter' ) ) {
             remove_role( 'prayer_supporter' );
         }
-        wpcom_vip_add_role(
+        add_role(
             'prayer_supporter', 'Prayer Supporter',
             [
                 'prayer_supporter' => true,
@@ -641,7 +641,7 @@ class Disciple_Tools_Roles
         if( get_role( 'registered' ) ) {
             remove_role( 'registered' );
         }
-        wpcom_vip_add_role(
+        add_role(
             'registered', 'Registered',
             [
                 // No capabilities to this role. Must be moved to another role for permission.
@@ -761,7 +761,7 @@ class Disciple_Tools_Roles
         remove_role( 'prayer_supporter' );
         remove_role( 'project_supporter' );
 
-        wpcom_vip_add_role(
+        add_role(
             'subscriber', 'Subscriber',
             [
                 'delete_others_posts'    => true,
@@ -791,7 +791,7 @@ class Disciple_Tools_Roles
             ]
         );
 
-        wpcom_vip_add_role(
+        add_role(
             'editor', 'Editor',
             [
                 'delete_others_posts'    => true,
@@ -821,7 +821,7 @@ class Disciple_Tools_Roles
                 'level_0'                => true,
             ]
         );
-        wpcom_vip_add_role(
+        add_role(
             'author', 'Author',
             [
                 'delete_posts'           => true,
@@ -834,7 +834,7 @@ class Disciple_Tools_Roles
             ]
         );
 
-        wpcom_vip_add_role(
+        add_role(
             'contributor', 'Contributor',
             [
                 'delete_posts' => true,
